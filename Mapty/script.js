924a699e07ffcd31ba99a11b8a7e8bfca539bd66
dimagutorka <dgutorka@gmail.com>
2025-03-26 16:28:11 +0200

    Refactor map in class

--- Mapty/script.js
+++ Mapty/script.js
@@ -13,54 +13,62 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 
 
+class App {
+    #map
+    #mapEvent
 
-let map, mapEvent
+    constructor() {
+        this._getPosition()
 
+        form.addEventListener('click', this._newWorkout.bind(this))
+        inputType.addEventListener('change', this._toggleEvaluationField)
+    }
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position){
-
-            const {latitude, longitude} = position.coords;
-            const coords = [latitude, longitude];
-            map = L.map('map').setView(coords, 13);
-
-            L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-                minZoom: 0, maxZoom: 20, attribution:
-                    '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> ' +
-                    '&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> ' +
-                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                ext: 'png'
-            }).addTo(map);
-
-            map.on('click', function(mapE) {
-                mapEvent = mapE
-                form.classList.remove('hidden')
-                inputDistance.focus()
+    _getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),
+                function() {alert('Could not get your location');
             })
-        },
+        }
+    }
 
-        function() {
-            alert('Could not get your location');
-        })
+    _loadMap(position) {
+        const {latitude, longitude} = position.coords;
+        const coords = [latitude, longitude];
+        this.#map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+            minZoom: 0, maxZoom: 20, attribution:
+                '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> ' +
+                '&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> ' +
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            ext: 'png'
+        }
+        ).addTo(this.#map);
+
+        this.#map.on('click', this._showForm.bind(this));
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE
+        form.classList.remove('hidden')
+        inputDistance.focus()
+    }
+
+    _toggleEvaluationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e) {
+        e.preventDefault();
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+        const {lat, lng} = this.#mapEvent.latlng;
+
+        L.marker([lat, lng]).addTo(this.#map).bindPopup(
+            L.popup({maxWidth: 250,  minWidth: 100, autoClose: false, closeOnClick: false, className: 'running-popup',}))
+            .setPopupContent('Label').openPopup();
+    }
 }
 
-
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
-    const {lat, lng} = mapEvent.latlng;
-
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({maxWidth: 250,  minWidth: 100, autoClose: false, closeOnClick: false, className: 'running-popup',}))
-        .setPopupContent('Label').openPopup();
-})
-
-
-inputType.addEventListener('change', function(e) {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-
-})
+const app = new App();
--- practice.js
+++ practice.js
@@ -37,3 +37,108 @@
 //
 //
 // const dog = new Mammal('ovcharka', 2005, 4)
+//
+//
+// const obj = {}
+//
+// Object.setPrototypeOf(obj, Mammal.prototype)
+// Mammal.call(obj, 'Alabai', 1901, 3)
+
+
+//
+// class Rectangle{
+//     width = 100
+//     height = 100
+//
+//
+//     area() {
+//         console.log('area of rectangle')
+//     }
+//
+//     get isSquare(){
+//         return this.width === this.height
+//     }
+// }
+//
+// const rect = new Rectangle();
+// rect.area();
+// console.log(rect.isSquare)
+
+
+// const User = function(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+//
+//
+// const Admin = function(name, age, isSuperAdmin) {
+//     User.call(this, name, age);
+//     this.isSuperAdmin = isSuperAdmin;
+// }
+//
+// Admin.prototype = Object.create(User.prototype);
+// Admin.prototype.constructor = Admin;
+//
+// Admin.prototype.hasAccess = function() {
+//     if (this.isSuperAdmin) {
+//         console.log('You have access to this resource')
+//         return true;
+//     }
+//     console.log('You have NO access to this resource');
+//     return false
+// }
+//
+//
+// const user1 = new User('Not admin', 100);
+// console.log(user1)
+//
+// const admin1 = new Admin('Not admin', 100, true);
+// console.log(admin1)
+// admin1.hasAccess();
+
+
+// class Counter {
+//     count = 0;
+//
+//     constructor(name) {
+//         this.name = name;
+//     }
+//
+//     increment = () => {
+//         this.count++;
+//         console.log(`${this.name}: ${this.count}`);
+//     };
+// }
+//
+// const test = new Counter('Dima');
+//
+// setTimeout(test.increment, 2000);
+
+
+// function outer() {
+//     this.name = 'outerThis';
+//
+//     const regular = function () {
+//         console.log('regular:', this.name);
+//     };
+//
+//     const arrow = () => {
+//         console.log('arrow:', this.name);
+//     };
+//
+//     return { regular, arrow };
+// }
+//
+// const obj = {
+//     name: 'realObj'
+// };
+//
+//
+// const { regular, arrow } = outer.call(obj); // outer вызван с this = obj
+//
+// console.log(regular, arrow)
+//
+//
+// regular(); // ❌ undefined (в strict mode)
+// arrow();   // ✅ 'realObj'
+
