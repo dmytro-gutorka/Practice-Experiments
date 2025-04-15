e134859df1012730893fc16093923ad23b095d76
dimagutorka <dgutorka@gmail.com>
2025-04-15 20:03:18 +0300

    Index types, Record

--- TS_inputs/intersection.ts
+++ TS_inputs/intersection.ts
@@ -25,15 +25,17 @@ type AccessedDatabaseData = DatabaseData & Status;
 
 // guards
 
-
-type FileSource = { path: string };
+type FileSource = { path: string, type: 'file' };
 const fileSource : FileSource = {
+  type: 'file',
   path: 'some/path/to/file.csv',
 }
 
-type DBSource = { connectionURL: string };
+
+type DBSource = { connectionURL: string, type: 'db' };
 const dbSource: DBSource = {
-  connectionURL: 'some-connection-url'
+  type: 'db',
+  connectionURL: 'some-connection-url',
 }
 
 
@@ -41,7 +43,64 @@ type Source = FileSource | DBSource
 
 
 function loadData(source: Source) {
-  if ('path' in source) {
+  if (source.type === 'file') return source.path
 
-  }
+  return source.connectionURL
 }
+
+
+// function overloads
+
+function getLength(val: any[]): number
+function getLength(val: string): string
+
+function getLength(val: string | any[]) {
+  if (typeof val === 'string') {
+    const numberOfWords = val.split(' ').length;
+    return `${numberOfWords}`
+  }
+  return val.length;
+}
+
+
+const numberOfWords = getLength('does this work?')
+const numItems = getLength(['Sports', 'Cookies'])
+
+
+// index types
+
+type DataStore = {
+  [prop: string]: boolean | string;
+}
+
+
+let store: DataStore = {
+  a: 'a',
+  b: 'b'
+};
+
+
+store.c = 'c'
+
+
+
+let roles = ['admin', 'guest', 'editor'] as const;
+
+const firstRole = roles[0]
+
+
+
+type CatName = "miffy" | "boris" | "mordred";
+
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" },
+};
+
+cats.boris;
